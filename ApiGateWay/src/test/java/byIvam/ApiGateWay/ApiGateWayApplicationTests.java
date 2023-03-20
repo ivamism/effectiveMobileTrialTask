5aedfd593a4f7d096f52ccfa3b8131759377715c
ivam_ <ivam@tut.by>
2023-03-20 20:57:43 +0300

    Create Api-Gateway

---
+++ ApiGateWay/src/test/java/byIvam/ApiGateWay/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package byIvam.ApiGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- AuthorizationService/src/main/java/byIvam/AuthorizationService/controller/TestController.java
+++ AuthorizationService/src/main/java/byIvam/AuthorizationService/controller/TestController.java
@@ -1,12 +1,16 @@
 package byIvam.AuthorizationService.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
     @GetMapping("/test")
     public String test(){
-        return "Test-test";
+        return id;
     }
 }
