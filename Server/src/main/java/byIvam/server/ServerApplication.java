b5e5afaef427e522f24c222d9514a5d040b0a47f
ivam <ivam@tut.by>
2023-03-21 22:34:01 +0300

    User Service controller

--- Server/src/main/java/byIvam/server/ServerApplication.java
+++ Server/src/main/java/byIvam/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package byIvam.namingserver;
+package byIvam.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class NamingServerApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NamingServerApplication.class, args);
+		SpringApplication.run(byIvam.server.ServerApplication.class, args);
 	}
 
 }
--- UserService/src/main/java/byIvam/UserService/controller/UserController.java
+++ UserService/src/main/java/byIvam/UserService/controller/UserController.java
@@ -2,6 +2,7 @@ package byIvam.UserService.controller;
 
 import byIvam.UserService.dto.UserDTO;
 import byIvam.UserService.model.User;
+import byIvam.UserService.service.UserService;
 import byIvam.UserService.service.UserServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,19 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserServiceImplementation userServiceImplementation;
+    private UserService userService;
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public User create(@RequestBody UserDTO userDTO) {
 
-        return userServiceImplementation.createUser(userDTO);
+        return userService.createUser(userDTO);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{userId}")
     @ResponseStatus(HttpStatus.OK)
     public User findById(@PathVariable long userId) {
-        User user = userServiceImplementation.findByID(userId);
+        User user = userService.findByID(userId);
         return user;
     }
 
